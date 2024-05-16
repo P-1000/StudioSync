f19916180784739ca77ac5869adc3fe16adee648
p-1000 <patchikarla.12108354@lpu.in>
2024-05-17 03:23:38 +0530

    Refactor: Remove dotenv config, use define in Vite config
    This commit refactors the frontend configuration to utilize the  option in the Vite config instead of using the  package.
    
    The following changes were made:
    
    - Added  package and configured it to load environment variables from  file.
    - Updated  to use  option to provide environment variables to the React app.
    - Removed the call to  from the  file.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom'
-import Login from './pages/Login'
-import Profile from './components/temp/Test'
-import Temp from './Temp'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Profile from "./components/temp/Test";
+import Temp from "./Temp";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Profile/>} />
-          <Route path="/login" element={<Login/>} />
-        <Route path="/oauth2callback" element={<Temp/>} />
+          <Route path="/" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/oauth2callback" element={<Temp />} />
         </Routes>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
-//dotenv 
-import dotenv from 'dotenv';
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
 <Auth0Provider
-    domain = {process.env.domain}
-    clientId = {process.env.clientId}
+    domain = {process.env.React_App_Domain}
+    clientId = {process.env.React_App_ClientId}
     authorizationParams={{
       redirect_uri: "http://localhost:5173"
     }}
